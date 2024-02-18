ab565acbdc5539885ddd54b2fa63cd2c6bcc4bb2
bl3utide <bl3utide@gmail.com>
2024-02-18 10:09:13 +0900

    add noexcept

--- Application/src/Main.hpp
+++ Application/src/Main.hpp
@@ -18,8 +18,8 @@ namespace ImGuiProject
 #define DEF_APP_DEV_YR      2024                // TODO fix dev year
 #define DEF_APP_TITLE       "ImGui Project"     // TODO fix app title
 
-std::string getAppVersion();
-std::string getAppCopyright();
-std::string getAppTitle();
+std::string getAppVersion() noexcept;
+std::string getAppCopyright() noexcept;
+std::string getAppTitle() noexcept;
 
 } // ImGuiApp
