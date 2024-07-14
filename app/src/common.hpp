e77f98a69f868d04376ddfc01a9f9332bcc97971
bl3utide <bl3utide@gmail.com>
2024-07-14 13:50:13 +0900

    refactoring logger

--- app/src/common.hpp
+++ app/src/common.hpp
@@ -32,8 +32,6 @@ namespace ImGuiProject
 #define DEF_APP_DEV_BY      "bl3utide"
 #define DEF_APP_DEV_YR      2024                // TODO fix dev year
 #define DEF_APP_TITLE       "ImGui Project"     // TODO fix app title
-#define LDEBUG LOGD
-#define LERROR LOGE_(1)
 
 const std::string APP_NAME = DEF_APP_NAME;
 } // ImGuiProject
